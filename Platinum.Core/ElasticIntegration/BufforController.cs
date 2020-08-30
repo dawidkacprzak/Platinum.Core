45330604402305ab53e5ff222cb9b5c47c243e98
capthapl <dawidkacprzak@icloud.com>
2020-08-30 21:17:21 +0200

    Added more logs and changed db ip

--- Platinum.Core/ElasticIntegration/BufforController.cs
+++ Platinum.Core/ElasticIntegration/BufforController.cs
@@ -42,7 +42,8 @@ namespace Platinum.Core.ElasticIntegration
 
         public void InsertOffer(string offer)
         {
-            client.Index(new ELBufforedOffers(offer), i => i.Index("buffered_offers"));
+            var response = client.Index(new ELBufforedOffers(offer), i => i.Index("buffered_offers"));
+            _logger.Info(JsonConvert.SerializeObject(response));
         }
 
         public void InsertOfferDetails(OfferDetails offerDetails)
@@ -84,7 +85,7 @@ namespace Platinum.Core.ElasticIntegration
                         )
                 )
             );
-
+            _logger.Info(JsonConvert.SerializeObject(searchResponse));
             return searchResponse.Documents.Count > 0;
         }
     }
