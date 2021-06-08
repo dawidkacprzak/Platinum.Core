d7c3ddfbc8ca0d2fa100d5820ecc07a6fbd0790b
Dawid Kacprzak <dawidkacprzak@icloud.com>
2021-06-08 06:20:50 +0200

    Update AllegroApiConfiguration.cs

--- Platinum.Core/AllegroAPIIntegration/AllegroApiConfiguration.cs
+++ Platinum.Core/AllegroAPIIntegration/AllegroApiConfiguration.cs
@@ -5,7 +5,7 @@ namespace Platinum.Core.AllegroAPIIntegration
     public static class AllegroApiConfiguration
     {
         public const string ALLEGRO_API_BASE_URL = "https://api.allegro.pl/";
-        public const string CLIENT_ID = "cea67e0ff8784d649b0c353e0a4a4e07";
-        public const string CLIENT_SECRET = "lt7hU0FeCtqnfOLO9AXsxzGVJh02oTkIfzrn6jYzn4jVrGhd1Om2wV2VnOaLgLCh";
+        public const string CLIENT_ID = "<secret>";
+        public const string CLIENT_SECRET = "<secret>";
     }
 }
